d4ce65151351fcc0ec69fb9543b4e3aba16eec2c
Kerlin Huang <ikerlin@msn.com>
2018-07-26 22:19:46 +0800

    事件处理重复token

---
+++ app/Listeners/PassportAccessTokenCreated.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Laravel\Passport\Events\AccessTokenCreated;
+use Laravel\Passport\Token;
+
+class PassportAccessTokenCreated
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $tokenIdArr = Token::where('id', '!=', $event->tokenId)
+            ->where('user_id', $event->userId)
+            ->pluck('id')->toArray();
+        \DB::table('oauth_refresh_tokens')->whereIn('access_token_id', $tokenIdArr)
+            ->delete();
+
+        Token::where('id', '!=', $event->tokenId)
+            ->where('user_id', $event->userId)
+            ->delete();
+    }
+}
